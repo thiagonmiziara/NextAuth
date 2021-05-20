77fd65dab4332d45ac4fba9fff03df8b9e5de228
thiago miziara <thiagonmiziara@gmail.com>
2021-05-20 19:40:29 -0300

    add toda logica de refreshhing token e  deslogando o usuário

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 import Router from "next/router";
-import {setCookie,parseCookies} from "nookies";
+import {setCookie,parseCookies, destroyCookie} from "nookies";
 
 import { api } from "../services/api";
 
@@ -27,6 +27,13 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData);
 
+export function signOut(){
+  destroyCookie(undefined, 'nextauth.token');
+  destroyCookie(undefined, 'nextauth.refreshToken');
+
+  Router.push('/');
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user; // retorna true se tiver algo e false se não tiver
@@ -39,6 +46,9 @@ export function AuthProvider({ children }: AuthProviderProps) {
         const {email, permissions, roles} = response.data;
 
         setUser({email, permissions, roles});
+      })
+      .catch(() =>{
+        signOut();
       });
     }
 
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,15 +2,15 @@ import { useContext, useEffect } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { api } from "../services/api";
 
-export default function Dashboard(){
+export default function Dashboard() {
+  const { user } = useContext(AuthContext);
 
-  const {user} = useContext(AuthContext);
+  useEffect(() => {
+    api
+      .get("/me")
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  }, []);
 
-  useEffect(()=>{
-    api.get('/me').then(response => console.log(response));
-  },[]);
-
-  return(
-    <h1>Dashboard: {user?.email}</h1>
-  );
+  return <h1>Dashboard: {user?.email}</h1>;
 }
