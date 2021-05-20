b42462093bbf87b592d3653d80c090286274dc32
thiago miziara <thiagonmiziara@gmail.com>
2021-05-20 17:05:05 -0300

    add component dashboar de navegando o usuária para uma nova rota

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,41 +1,57 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
+import Router from "next/router";
 import { api } from "../services/api";
 
-type SignInCredentials ={
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
+
+type SignInCredentials = {
   email: string;
   password: string;
-}
+};
 
-type AuthContextData ={
+type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>;
+  user: User;
   isAuthenticated: boolean;
 };
 
-type AuthProviderProps ={
+type AuthProviderProps = {
   children: ReactNode;
-}
+};
 
 export const AuthContext = createContext({} as AuthContextData);
 
-export function AuthProvider({children}: AuthProviderProps){
-  const isAuthenticated = false;
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user; // retorna true se tiver algo e false se não tiver
 
-  async function signIn({email, password}: SignInCredentials){
-   try{ 
-    const response = await api.post('sessions',{
-      email,
-      password,
-    });
-    console.log(response.data);
-   }catch (err){
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post("sessions", {
+        email,
+        password,
+      });
+
+      const { permissions, roles } = response.data;
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      Router.push("/dashboard");
+    } catch (err) {
       console.log(err);
-   }
+    }
   }
 
-
-  return(
-      <AuthContext.Provider value={{signIn, isAuthenticated}}>
-        {children}
-      </AuthContext.Provider>
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
