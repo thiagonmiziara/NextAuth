803c62b0a59711a29e791706f1ca986376d36628
thiago miziara <thiagonmiziara@gmail.com>
2021-05-22 19:53:12 -0300

    add requisição pelo de toke pelo lado server

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -2,7 +2,8 @@ import { createContext, ReactNode, useEffect, useState } from "react";
 import Router from "next/router";
 import {setCookie,parseCookies, destroyCookie} from "nookies";
 
-import { api } from "../services/api";
+import { setupAPIClient } from "../services/api";
+import { api } from "../services/apiClient";
 
 type User = {
   email: string;
---
+++ services/apiClient.ts
@@ -0,0 +1,4 @@
+import {setupAPIClient} from "./api";
+
+ //utilizar somente do lando do client 
+export const api =setupAPIClient();
